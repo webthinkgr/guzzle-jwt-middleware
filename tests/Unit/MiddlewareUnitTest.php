aba03795b29527cfc87fd78ed47f51a5b04579ab
George Mponos <gmponos@gmail.com>
2017-08-02 22:14:28 +0300

    Fix unit test

--- tests/Unit/MiddlewareUnitTest.php
+++ tests/Unit/MiddlewareUnitTest.php
@@ -39,7 +39,7 @@ class MiddlewareUnitTest extends \PHPUnit_Framework_TestCase
             ],
         ]);
 
-        $this->expectException(BadCredentialException::class);
+        $this->setExpectedException(BadCredentialException::class);
         $promise->wait();
     }
 
